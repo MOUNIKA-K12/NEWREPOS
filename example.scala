8e93e4e837eefd94d152692dd4e306cc5e0f7f73
MOUNIKA-K12 <130341330+MOUNIKA-K12@users.noreply.github.com>
2023-04-10 11:58:13 +0530

    Add files via upload

---
+++ example.scala
@@ -0,0 +1,11 @@
+object myObject {
+    def main(args: Array[String]) {
+        var _value1 = 54
+        var value_2 = 65
+    
+        var val_+ = _value1 + value_2
+    
+        println("The sum is " + (val_+))
+    }
+}
+
